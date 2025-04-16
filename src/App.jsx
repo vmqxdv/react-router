da9f93d089b45ee7d0e15ce2ffa520b12695e3b6
vmqxdv <vquintilio@gmail.com>
2025-04-16 17:03:03 +0200

    feat: Impostate route base

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage.jsx';
+import AboutPage from './pages/About.jsx';
+import PostsPage from './pages/Posts.jsx';
+
+
 export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={AboutPage} />
+        <Route path='/posts' Component={PostsPage} />
+      </Routes>
+    </BrowserRouter>
   )
 };
