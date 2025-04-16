4a2c582b9407f46752fe671f6f3b022754b74a4b
vmqxdv <vquintilio@gmail.com>
2025-04-16 17:33:23 +0200

    feat: Aggiunto lista posts

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+  }, []);
+
+  const displayPosts = (posts) => {
+    return posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ))
+  };
+
   return (
-    <div className='posts'>posts</div>
-  )
+    <div className='posts'>
+      {displayPosts(posts)}
+    </div>
+  );
 };
